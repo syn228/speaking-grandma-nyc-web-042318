c1253659ce89af5f1be2c254c3b1b542809fb8c4
Aaron Na <syn228@nyu.edu>
2018-04-07 21:11:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(input)
   if input.upcase
     return "NO, NOT SINCE 1938!"
-  elseif input.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+  elseif input == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
