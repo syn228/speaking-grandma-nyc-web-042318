ae83d42a0987b627c2b620c9fd8d18eefa22e4bc
Aaron Na <syn228@nyu.edu>
2018-04-07 21:18:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,9 @@
 def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif (input == "Hi Nana, how are you?")
-    return "HUH?! SPEAK UP, SONNY!"
-  elseif input == "Hi!"
-    return "HUH?! SPEAK UP, SONNY!"
-  elseif input == "WHAT DID YOU EAT TODAY?"
+  elseif input.upcase
     return "NO, NOT SINCE 1938!"
-  elseif input == "WHAT?"
-    return "NO, NOT SINCE 1938!"
+  elseif input.downcase
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
