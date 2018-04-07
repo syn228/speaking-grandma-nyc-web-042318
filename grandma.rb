4ffd428cca5334cb2a4130ff444262d745538f65
Aaron Na <syn228@nyu.edu>
2018-04-07 21:01:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,6 @@ def speak_to_grandma(dialogue)
     return "I LOVE YOU TOO PUMPKIN!"
   elseif dialogue = dialogue.upcase
     return "NO, NOT SINCE 1938!"
-  elseif dialogue = dialogue.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+  else return "HUH?! SPEAK UP, SONNY!"
   end
 end
